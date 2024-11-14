15899750fa56865f0dcc21b998f1288f0f0f1645
Paveen <paveenhuang@gmail.com>
2024-11-14 19:59:40 +0800

    Update text_otf_module.py

--- src/models/text_otf_module.py
+++ src/models/text_otf_module.py
@@ -111,8 +111,8 @@ class LanguageTaskOnTheFlyLitModule(LightningModule):
     def module_step(self, batch: dict, batch_idx: int):
         if isinstance(self.llm, ChatGPT) or isinstance(self.llm, PaLM):
             return self.module_step_chatgpt(batch, batch_idx)
-        elif hasattr(self.llm, 'model_path') and "llama3" in self.llm.model_path.lower():
-            return self.module_step_llama(batch, batch_idx)
+        # elif hasattr(self.llm, 'model_path') and "llama3" in self.llm.model_path.lower():
+        #     return self.module_step_llama(batch, batch_idx)
 
         # one method to do it all
         text = batch["text"]
