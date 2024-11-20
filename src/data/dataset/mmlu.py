d4387d81780c79fad6b827c0ab3760e87ff112a1
Paveen <paveenhuang@gmail.com>
2024-11-20 22:00:30 +0800

    Update mmlu.py

--- src/data/dataset/mmlu.py
+++ src/data/dataset/mmlu.py
@@ -1,68 +1,71 @@
 from typing import Any
-
+import numpy as np
 from datasets import load_dataset
 import torch
 from torch.utils.data import Dataset
 
-TASKS = [
-"high_school_european_history",
-"business_ethics",
-"clinical_knowledge",
-"medical_genetics",
-"high_school_us_history",
-"high_school_physics",
-"high_school_world_history",
-"virology",
-"high_school_microeconomics",
-"econometrics",
-"college_computer_science",
-"high_school_biology",
-"abstract_algebra",
-"professional_accounting",
-"philosophy",
-"professional_medicine",
-"nutrition",
-"global_facts",
-"machine_learning",
-"security_studies",
-"public_relations",
-"professional_psychology",
-"prehistory",
-"anatomy",
-"human_sexuality",
-"college_medicine",
-"high_school_government_and_politics",
-"college_chemistry",
-"logical_fallacies",
-"high_school_geography",
-"elementary_mathematics",
-"human_aging",
-"college_mathematics",
-"high_school_psychology",
-"formal_logic",
-"high_school_statistics",
-"international_law",
-"high_school_mathematics",
-"high_school_computer_science",
-"conceptual_physics",
-"miscellaneous",
-"high_school_chemistry",
-"marketing",
-"professional_law",
-"management",
-"college_physics",
-"jurisprudence",
-"world_religions",
-"sociology",
-"us_foreign_policy",
-"high_school_macroeconomics",
-"computer_security",
-"moral_scenarios",
-"moral_disputes",
-"electrical_engineering",
-"astronomy",
-"college_biology",
-]
+# TASKS = [
+# "high_school_european_history",
+# "business_ethics",
+# "clinical_knowledge",
+# "medical_genetics",
+# "high_school_us_history",
+# "high_school_physics",
+# "high_school_world_history",
+# "virology",
+# "high_school_microeconomics",
+# "econometrics",
+# "college_computer_science",
+# "high_school_biology",
+# "abstract_algebra",
+# "professional_accounting",
+# "philosophy",
+# "professional_medicine",
+# "nutrition",
+# "global_facts",
+# "machine_learning",
+# "security_studies",
+# "public_relations",
+# "professional_psychology",
+# "prehistory",
+# "anatomy",
+# "human_sexuality",
+# "college_medicine",
+# "high_school_government_and_politics",
+# "college_chemistry",
+# "logical_fallacies",
+# "high_school_geography",
+# "elementary_mathematics",
+# "human_aging",
+# "college_mathematics",
+# "high_school_psychology",
+# "formal_logic",
+# "high_school_statistics",
+# "international_law",
+# "high_school_mathematics",
+# "high_school_computer_science",
+# "conceptual_physics",
+# "miscellaneous",
+# "high_school_chemistry",
+# "marketing",
+# "professional_law",
+# "management",
+# "college_physics",
+# "jurisprudence",
+# "world_religions",
+# "sociology",
+# "us_foreign_policy",
+# "high_school_macroeconomics",
+# "computer_security",
+# "moral_scenarios",
+# "moral_disputes",
+# "electrical_engineering",
+# "astronomy",
+# "college_biology",
+# ]
+
+
+TASKS = ["management"]
 
 
 class MMLU(Dataset):
@@ -140,37 +143,37 @@ if __name__ == "__main__":
 
     print(len(TASKS))
     cache_dir = "/data2/paveen/RolePlaying/.cache"
-    # # compute imbalance
-    # with open("mmlu_stats.txt", "w") as o:
-    #     for t in TASKS:
-    #         print(t)
-    #         sc = MMLU(t, cache_dir=cache_dir, split="test")
-    #         targets = [item["label"] for item in sc]
-    #         values, counts = np.unique(targets, return_counts=True)
-    #         counts = counts * 100.0 / len(targets)
-    #         print(f"Task {t}: {counts}", file=o)
+    # compute imbalance
+    with open("mmlu_stats.txt", "w") as o:
+        for t in TASKS:
+            print(t)
+            sc = MMLU(t, cache_dir=cache_dir, split="test")
+            targets = [item["label"] for item in sc]
+            values, counts = np.unique(targets, return_counts=True)
+            counts = counts * 100.0 / len(targets)
+            print(f"Task {t}: {counts}", file=o)
 
-    # # compute number of instances in test sets
-    # max_len = 0
-    # max_t = None
-    # min_len = 100000000
-    # min_t = None
-    # all_lens = []
-    # for t in TASKS:
-    #     sc = MMLU(t, cache_dir=cache_dir, split="test")
-    #     all_lens.append(len(sc))
-    #     if len(sc) > max_len:
-    #         max_len = len(sc)
-    #         max_t = t
-    #     if len(sc) < min_len:
-    #         min_len = len(sc)
-    #         min_t = t
+    # compute number of instances in test sets
+    max_len = 0
+    max_t = None
+    min_len = 100000000
+    min_t = None
+    all_lens = []
+    for t in TASKS:
+        sc = MMLU(t, cache_dir=cache_dir, split="test")
+        all_lens.append(len(sc))
+        if len(sc) > max_len:
+            max_len = len(sc)
+            max_t = t
+        if len(sc) < min_len:
+            min_len = len(sc)
+            min_t = t
 
-    # print(f"Max dataset  {max_t} with len {max_len}")
-    # print(f"Min dataset  {min_t} with len {min_len}")
-    # with open("mmlu_lens.txt", "w") as o:
-    #     for i, t in enumerate(TASKS):
-    #         print(f"{t}: {all_lens[i]}", file=o)
+    print(f"Max dataset  {max_t} with len {max_len}")
+    print(f"Min dataset  {min_t} with len {min_len}")
+    with open("mmlu_lens.txt", "w") as o:
+        for i, t in enumerate(TASKS):
+            print(f"{t}: {all_lens[i]}", file=o)
     
     # print tasks
     
