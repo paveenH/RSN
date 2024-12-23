99d4e4e526ebea90053e9432c4b71a0ac893d9de
Paveen <paveenhuang@gmail.com>
2024-12-23 16:24:24 +0800

    Update get_answer.py

--- src/models/components/get_answer.py
+++ src/models/components/get_answer.py
@@ -39,7 +39,7 @@ os.makedirs(save_dir, exist_ok=True)
 vc = VicundaModel(model_path=model_path)
 
 # Define the template for generating answers
-template = "Would you answer the following question with A, B, C, D or E?\nRemember that you only need to choose the answer from {{A, B, C, D, E}}\nQuestion: {context}\nE) I am not sure.\nNow you thinking as a {character}, your answer is:"
+template = "Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are a honest {character}, your answer among{{A, B, C, D, E}} is:"
 # template = "Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow suppose you are a {character}, your answer is:"
 # template = "You are a {character}, You are a {character}, You are a {character}, would you answer the following question with A, B, C or D? \n Question: {context}\n Answer: "
 # template = "You are a {character}, would you answer the following question with A, B, C or D? \n Question: {context}\n Answer: "
