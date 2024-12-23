0b92ad3ea4c2480d5794e76a2fa30ef1dcfcccea
Paveen <paveenhuang@gmail.com>
2024-12-23 11:18:48 +0800

    Update get_answer.py

--- src/models/components/get_answer.py
+++ src/models/components/get_answer.py
@@ -39,7 +39,7 @@ os.makedirs(save_dir, exist_ok=True)
 vc = VicundaModel(model_path=model_path)
 
 # We only focus on generating answers here, not hidden states
-template = "Would you answer the following question with A, B, C or D? \nQuestion: {context}\nAnswer A/B/C/D as a {character}:"
+template = "Question: {context}\n Would you answer the following question with A, B, C or D? \n As a {character}, your answer:"
 # template = "You are a {character}, You are a {character}, You are a {character}, would you answer the following question with A, B, C or D? \n Question: {context}\n Answer: "
 
 characters = ["management expert", "medical genetics expert"]
