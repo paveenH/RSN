bf72689c863cf19922ffe5f02781b4391e7b1cbf
Paveen <paveenhuang@gmail.com>
2024-12-23 15:31:13 +0800

    Update get_answer.py

--- src/models/components/get_answer.py
+++ src/models/components/get_answer.py
@@ -45,7 +45,7 @@ template = "Would you answer the following question with A, B, C, D or E?\nQuest
 # template = "You are a {character}, would you answer the following question with A, B, C or D? \n Question: {context}\n Answer: "
 
 # Define the list of characters
-characters = ["management expert", "medical genetics expert"]
+characters = ["management expert", "medical genetics expert", "abstract algebra expert"]
 
 # Load JSON data
 print(f"Loading JSON data from {json_path}")
