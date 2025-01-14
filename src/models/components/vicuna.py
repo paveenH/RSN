4f0c16c24a0c4a5276f87eb3152f06165a792710
Paveen <paveenhuang@gmail.com>
2025-01-14 20:38:21 +0800

    Update vicuna.py

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -38,7 +38,7 @@ class VicundaModel:
         else:
             self.system_prompt = None
         
-        self.template = "Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among{{A, B, C, D, E}} is: "
+        self.template = "Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among [A, B, C, D, E] is: "
         
         if quantized:
             bnb_config = BitsAndBytesConfig(
