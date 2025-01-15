08805f2ed40cf3006e0266689bbecf858ad307af
Paveen <paveenhuang@gmail.com>
2025-01-15 17:08:39 +0800

    Update download.py

--- download.py
+++ download.py
@@ -60,9 +60,25 @@ def download_model(model_name, model_path):
 if __name__ == "__main__":
     models_to_download = [
         {
-            "model_name": "mistralai/Mistral-7B-Instruct-v0.3",
-            "model_path": "shared/mistral/7B",
+            "model_name": "google/gemma-2-9b",
+            "model_path": "shared/google/gemma-2-9b",
         },
+        {
+            "model_name": "google/gemma-2-2b",
+            "model_path": "shared/google/gemma-2-2b",
+        },
+        {
+            "model_name": "allenai/OLMo-7B",
+            "model_path": "shared/allenai/OLMo-7B",
+        },
+        {
+            "model_name": "allenai/OLMo-1B",
+            "model_path": "shared/allenai/OLMo-1B",
+        },
+        # {
+        #     "model_name": "mistralai/Mistral-7B-Instruct-v0.3",
+        #     "model_path": "shared/mistral/7B",
+        # },
         # {
         #     "model_name": "Qwen/Qwen2.5-7B-Instruct",
         #     "model_path": "shared/qwen2.5/7B",
