3491f7e97acaa1ad1c41166a547608d9370a7923
Paveen <paveenhuang@gmail.com>
2025-01-22 11:31:05 +0800

    Update get_answer_regenerate.py

--- src/models/components/get_answer_regenerate.py
+++ src/models/components/get_answer_regenerate.py
@@ -20,7 +20,7 @@ parser = argparse.ArgumentParser(description="Run VicundaModel on a specific tas
 parser.add_argument("task_size_model", type=str, help="The task and size as a combined argument.")
 args = parser.parse_args()
 
-task, size, model_name = args.task_size.split()
+task, size, model_name = args.task_size_model.split()
 
 # Path definition
 model_path = f"/data2/paveen/RolePlaying/shared/{model_name}/{size}"
