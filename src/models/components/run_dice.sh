aa170333a6d69463410e61c686dc4161445cde1f
Paveen <paveenhuang@gmail.com>
2025-02-04 17:24:55 +0800

    Create run_dice.sh

---
+++ src/models/components/run_dice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# model and size
+declare -A model_sizes
+model_sizes["llama3"]="1B 3B 8B"
+model_sizes["mistral"]="7B"
+model_sizes["qwen2.5"]="0.5B 3B 7B"
+model_sizes["phi"]="3.8B"
+
+for model in "${!model_sizes[@]}"; do
+    for size in ${model_sizes[$model]}; do
+        echo "Running kl_task.py for model: $model, size: $size"
+        python3 kl_task.py "$model" "$size"
+    done
+done
