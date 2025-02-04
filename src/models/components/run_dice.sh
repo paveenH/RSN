a01f8ebbf542c8bd5efde09783623c582d654aff
Paveen <paveenhuang@gmail.com>
2025-02-04 21:53:32 +0800

    Update run_dice.sh

--- src/models/components/run_dice.sh
+++ src/models/components/run_dice.sh
@@ -5,10 +5,11 @@ declare -A model_sizes
 model_sizes["llama3"]="1B 3B 8B"
 model_sizes["mistral"]="7B"
 model_sizes["qwen2.5"]="0.5B 3B 7B"
+model_sizes["phi"]="3.8B"
 
 for model in "${!model_sizes[@]}"; do
     for size in ${model_sizes[$model]}; do
-        echo "Running kl_task.py for model: $model, size: $size"
+        echo "Running ks_task.py for model: $model, size: $size"
         python3 kl_task.py "$model" "$size"
     done
 done
