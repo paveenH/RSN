18cda678c15643762418c36e6e08656fcf305167
Paveen <paveenhuang@gmail.com>
2025-02-05 16:52:49 +0800

    Update run_reanswer.sh

--- src/models/components/run_reanswer.sh
+++ src/models/components/run_reanswer.sh
@@ -4,7 +4,34 @@
 
 # Define the list of tasks
 TASKS=(
+"abstract_algebra"
+"anatomy"
+"astronomy"
+"business_ethics"
+"clinical_knowledge"
+"college_biology"
+"college_chemistry"
+"college_computer_science"
+"college_medicine"
+"college_mathematics"
+"college_physics"
+"computer_security"
+"conceptual_physics"
+"econometrics"
+"electrical_engineering"
+"elementary_mathematics"
+"formal_logic"
+"global_facts"
+"high_school_biology"
+"high_school_chemistry"
+"high_school_computer_science"
+"high_school_european_history"
+"high_school_geography"
+"high_school_government_and_politics"
+"high_school_macroeconomics"
+"high_school_mathematics"
 "high_school_microeconomics"
+"high_school_physics"
 "high_school_psychology"
 "high_school_statistics"
 "high_school_us_history"
@@ -16,13 +43,31 @@ TASKS=(
 "logical_fallacies"
 "machine_learning"
 "management"
+"marketing"
+"medical_genetics"
+"miscellaneous"
+"moral_disputes"
+"moral_scenarios"
+"nutrition"
+"philosophy"
+"prehistory"
+"professional_accounting"
+"professional_law"
+"professional_medicine"
+"professional_psychology"
+"public_relations"
+"security_studies"
+"sociology"
+"us_foreign_policy"
+"virology"
+"world_religions"
 )
 
 # Define the list of model sizes, models, tops, and alphas
-SIZES=("3.8B")
-MODELS=("phi")
-TOPS=("15") 
-ALPHAS=("5")
+SIZES=("7B")
+MODELS=("mistral")
+TOPS=("20") 
+ALPHAS=("1")
 
 # Get the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
