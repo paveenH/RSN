4c130607275579a660e8174e6b0f6454b41fe113
Paveen <paveenhuang@gmail.com>
2025-02-22 18:40:27 +0800

    Update run_reanswer_layer.sh

--- src/models/components/run_reanswer_layer.sh
+++ src/models/components/run_reanswer_layer.sh
@@ -67,11 +67,11 @@ TASKS=(
 
 SIZES=("8B")
 MODELS=("llama3")
-TOPS=("4096")
+TOPS=("20")
 ALPHAS=("1")
 
 # Define explicit start-end pairs (1-based indexing)
-START_END_PAIRS=("31 32")
+START_END_PAIRS=("10 31")
 
 JOBS=1
 
