6da1a6e8ca767de5279900cdb0cb11a6f0b2f64d
Paveen <paveenhuang@gmail.com>
2025-02-22 16:46:53 +0800

    Last layer experiment

--- src/models/components/run_reanswer_layer_copy.sh
+++ src/models/components/run_reanswer_layer_copy.sh
@@ -67,11 +67,11 @@ TASKS=(
 
 SIZES=("8B")
 MODELS=("llama3")
-TOPS=("20")
+TOPS=("640")
 ALPHAS=("1")
 
 # Define explicit start-end pairs (1-based indexing)
-START_END_PAIRS=("10 31")
+START_END_PAIRS=("31 32")
 
 JOBS=1
 
