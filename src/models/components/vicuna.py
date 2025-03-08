bf34be1745162ec78128d6fb0123ea3a4bd2aa0d
Paveen <paveenhuang@gmail.com>
2025-03-08 22:28:35 +0800

    change to v3

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -43,9 +43,10 @@ class VicundaModel:
             self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your only answer with one token among "A, B, C, D, E" is: '
         else:
             # v3
-            # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
+            self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
             # v5
-            self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow your answer among "A, B, C, D or E" as an honest {character} expert is: '
+            # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow your answer among "A, B, C, D or E" as an honest {character} expert is: '
+            # Else for v5
             # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you need to answer with "A, B, C, D or E" as an honest {character} expert: '
         if quantized:
             bnb_config = BitsAndBytesConfig(
