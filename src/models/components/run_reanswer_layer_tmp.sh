8a1939cc6de9f3c9b685be0e5b59e55ddac12c21
Paveen <paveenhuang@gmail.com>
2025-03-23 12:36:22 +0800

    Update run_reanswer_layer_tmp.sh

--- src/models/components/run_reanswer_layer_tmp.sh
+++ src/models/components/run_reanswer_layer_tmp.sh
@@ -64,8 +64,8 @@ TASKS=(
 
 SIZES=("8B")
 MODELS=("llama3")
-TOPS=("10" "20")
-ALPHAS=("1.1" "1.3" "1.5" "2" "3" )
+TOPS=("20")
+ALPHAS=("4" "5" "6")
 
 # Define explicit start-end pairs (1-based indexing)
 START_END_PAIRS=("11 21")
