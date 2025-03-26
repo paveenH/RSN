81863bae52e1c0ee5001c7b25d84b83feaff072c
Paveen <paveenhuang@gmail.com>
2025-03-26 17:16:18 +0800

    Update vicuna.py

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -49,12 +49,10 @@ class VicundaModel:
             # v3 4-options
             # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character} expert, your answer among "A, B, C, D" is: '
             # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character}, your answer among "A, B, C, D" is: '
-            self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are {character}, your answer among "A, B, C, D" is: '
-            # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nYour answer among "A, B, C, D" is: '
+            # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are {character}, your answer among "A, B, C, D" is: '
+            # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nYour answer among "A, B, C, D" is: ' # No-role
             # v5
             # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow your answer among "A, B, C, D or E" as an honest {character} expert is: '
-            # Else for v5
-            # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you need to answer with "A, B, C, D or E" as an honest {character} expert: '
             # v6 4-options
             # self.template = 'Now you are a {character}, would you answer the following question with A, B, C or D?\nQuestion: {context}\nYour answer among "A, B, C, D" is: '
             # v6
