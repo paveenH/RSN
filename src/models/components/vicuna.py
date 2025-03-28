4b8ae99fb8347e733c2114d647a13cf5e06c8f8f
Paveen <paveenhuang@gmail.com>
2025-03-28 15:21:53 +0800

    Update vicuna.py

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -48,8 +48,7 @@ class VicundaModel:
             # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} student, your answer among "A, B, C, D, E" is: '
             # v3 4-options
             # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character} expert, your answer among "A, B, C, D" is: '
-            # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character}, your answer among "A, B, C, D" is: '
-            self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are {character}, your answer among "A, B, C, D" is: '
+            self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character}, your answer among "A, B, C, D" is: '
             # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nYour answer among "A, B, C, D" is: ' # No-role
             # v5
             # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow your answer among "A, B, C, D or E" as an honest {character} expert is: '
