ad1f8b20ebeb2fefc9935d885ed5162eabf4f4e4
Paveen <paveenhuang@gmail.com>
2025-04-09 22:41:26 +0800

    Update get_answer_regenerate_layer.py

--- src/models/components/get_answer_regenerate_layer.py
+++ src/models/components/get_answer_regenerate_layer.py
@@ -115,7 +115,7 @@ def main():
     model_path = f"/data2/paveen/RolePlaying/shared/{model_name}/{size}"
     json_path = os.path.join("/data2/paveen/RolePlaying/src/models/components/mmlu", f"{task}.json")
     matrix_path = f"/data2/paveen/RolePlaying/src/models/components/hidden_states_v3_mean/{model_name}"
-    save_dir = os.path.join(f"/data2/paveen/RolePlaying/src/models/components/answer_v3_4ops/{model_name}_{alpha}")
+    save_dir = os.path.join(f"/data2/paveen/RolePlaying/src/models/components/answer_4ops_mdf/{model_name}_{alpha}")
     os.makedirs(save_dir, exist_ok=True)
 
     # Load difference matrices with exception handling
