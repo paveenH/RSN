3ba75394a8aa80f0f5b35856ae86caa96f0c26cb
Paveen <paveenhuang@gmail.com>
2025-04-16 10:50:37 +0800

    Update vicuna.py

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -41,11 +41,11 @@ class VicundaModel:
         # v4
         # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your only answer with one token among "A, B, C, D, E" is: '
         # v3
-        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
+        self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
         # v3 students
         # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} , your answer among "A, B, C, D, E" is: '
         # v3 no role
-        self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nYour answer among "A, B, C, D, E" is: '
+        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nYour answer among "A, B, C, D, E" is: '
         # v3 4-options
         # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character} expert, your answer among "A, B, C, D" is: '
         # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character}, your answer among "A, B, C, D" is: '
