166f900d4d9253481cc3e49030c0c142a7037cda
Paveen <paveenhuang@gmail.com>
2025-05-14 11:42:40 +0800

    Update vicuna.py

--- src/models/components/vicuna.py
+++ src/models/components/vicuna.py
@@ -39,10 +39,10 @@ class VicundaModel:
         # v2
         # self.template = "Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among (A, B, C, D, E) is: "
         # v4 5-options
-        self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your only answer with one token among "A, B, C, D, E" is: '
+        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your only answer with one token among "A, B, C, D, E" is: '
         # v3 5-options
-        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
-        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} , your answer among "A, B, C, D, E" is: ' # v3 students
+        self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character} expert, your answer among "A, B, C, D, E" is: '
+        # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nNow you are an honest {character}, your answer among "A, B, C, D, E" is: ' # v3 students
         # self.template = 'Would you answer the following question with A, B, C, D or E?\nQuestion: {context}\nE) I am not sure.\nYour answer among "A, B, C, D, E" is: ' # v3 no role
         # v3 4-options
         # self.template = 'Would you answer the following question with A, B, C or D?\nQuestion: {context}\nNow you are a {character} expert, your answer among "A, B, C, D" is: '
