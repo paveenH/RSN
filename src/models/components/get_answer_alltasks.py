bd5ee8264d5ab9db21b653ff3ff87ed0f632e7a0
Paveen <paveenhuang@gmail.com>
2025-05-26 11:39:49 +0800

    Update get_answer_alltasks.py

--- src/models/components/get_answer_alltasks.py
+++ src/models/components/get_answer_alltasks.py
@@ -5,9 +5,6 @@ Batch answer-generation & accuracy script
 Replaces per-task shell calls by looping over TASKS list in one run.
 Author: paveenhuang
 """
-
-import patch_transformers
-
 import os
 import json
 import re
