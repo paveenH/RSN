4847e5930665c74e43b9faa2b4f0ffb17491fe6e
Paveen <paveenhuang@gmail.com>
2025-06-27 16:35:22 +0800

    Update get_answer_alltasks.py

--- get_answer_alltasks.py
+++ get_answer_alltasks.py
@@ -74,7 +74,7 @@ TASKS = [
 
 
 MODEL = "llama3"
-SIZE = "8B"
+SIZE = "3B"
 
 # fixed paths
 PATH_MMLU = "/data2/paveen/RolePlaying/components/mmlu"
@@ -82,8 +82,8 @@ SAVE_BASE = "/data2/paveen/RolePlaying/components/answer_orig"
 
 # MODEL_DIR = f"/data2/paveen/RolePlaying/shared/{MODEL}/{SIZE}"
 # MODEL_DIR = "NousResearch/Hermes-3-Llama-3.2-3B"
-# MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"
-MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
+MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"  
+# MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
 
 print (MODEL_DIR)
 
