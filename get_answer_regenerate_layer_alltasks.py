cace4da6e60e7a07577642fc510372461b94e7e6
Paveen <paveenhuang@gmail.com>
2025-06-27 10:19:42 +0800

    Update get_answer_regenerate_layer_alltasks.py

--- get_answer_regenerate_layer_alltasks.py
+++ get_answer_regenerate_layer_alltasks.py
@@ -34,7 +34,7 @@ MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
 print(MODEL_DIR)
 
 TOP = 20
-ALPHAS_START_END_PAIRS = [[1, (1,32)], [4, (14,22)]]
+ALPHAS_START_END_PAIRS = [[1, (1,32)], [4, (11,20)]]
 
 SHORT = 1
 LONG = 12
