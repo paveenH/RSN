45edc75654ae69888d8d05d04f0a57507946c811
Paveen <paveenhuang@gmail.com>
2025-06-30 21:09:15 +0800

    Update get_answer_regenerate_layer_alltasks.py

--- get_answer_regenerate_layer_alltasks.py
+++ get_answer_regenerate_layer_alltasks.py
@@ -19,15 +19,17 @@ LABEL_MAPPING = ["A", "B", "C", "D"]
 # === Configuration ===
 TASKS = ga.TASKS
 
-MODEL = "openchat"
+MODEL = "zephyr"
 SIZE = "7B"
 TYPE = "non"
 # 
 # MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
 # MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"
 # MODEL_DIR = "NousResearch/Hermes-3-Llama-3.2-3B"
-MODEL_DIR = "openchat/openchat_3.5"
+# MODEL_DIR = "openchat/openchat_3.5"
 # MODEL_DIR =  "mistralai/Mistral-7B-Instruct-v0.3"
+MODEL_DIR = "HuggingFaceH4/zephyr-7b-beta"
+
 
 print(MODEL_DIR)
 
