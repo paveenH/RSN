18117f062d7bb9ceeb26472b05d264708e0d9420
Paveen <paveenhuang@gmail.com>
2025-06-30 21:43:38 +0800

    Update get_answer_regenerate_layer_alltasks.py

--- get_answer_regenerate_layer_alltasks.py
+++ get_answer_regenerate_layer_alltasks.py
@@ -19,23 +19,24 @@ LABEL_MAPPING = ["A", "B", "C", "D"]
 # === Configuration ===
 TASKS = ga.TASKS
 
-MODEL = "openchat"
+MODEL = "mistral"
 SIZE = "7B"
 TYPE = "non"
 # 
 # MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
 # MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"
 # MODEL_DIR = "NousResearch/Hermes-3-Llama-3.2-3B"
-MODEL_DIR = "openchat/openchat_3.5"
+# MODEL_DIR = "openchat/openchat_3.5"
 # MODEL_DIR =  "mistralai/Mistral-7B-Instruct-v0.3"
+MODEL_DIR =  "mistralai/Mistral-7B-v0.3"
 # MODEL_DIR = "HuggingFaceH4/zephyr-7b-beta"
 
 
 print(MODEL_DIR)
 
 TOP = 20
-# ALPHAS_START_END_PAIRS = [[1, (1,33)], [4, (14,22)]]
-ALPHAS_START_END_PAIRS = [[3, (14,22)]]
+ALPHAS_START_END_PAIRS = [[1, (1,33)], [4, (14,22)]]
+# ALPHAS_START_END_PAIRS = [[3, (14,22)]]
 
 
 SHORT = 2
@@ -48,7 +49,7 @@ STEP = 16
 MAT_DIR = f"/data2/paveen/RolePlaying/components/hidden_states_mean/{MODEL}_non"
 print("import hidden states difference from ", MAT_DIR)
 MMLU_DIR = "/data2/paveen/RolePlaying/components/mmlu"
-SAVE_DIR = "/data2/paveen/RolePlaying/components/answer_modified_non"
+SAVE_DIR = "/data2/paveen/RolePlaying/components/answer_modified_base"
 
 
 # === Helper functions ===
