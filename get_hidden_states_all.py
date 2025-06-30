c4f26b39589d3cc078c15803135a8f42a7b3ceed
Paveen <paveenhuang@gmail.com>
2025-06-30 14:45:44 +0800

    Update get_hidden_states_all.py

--- get_hidden_states_all.py
+++ get_hidden_states_all.py
@@ -19,8 +19,8 @@ TYPE = "non"
 
 # MODEL_DIR = os.path.join("/data2/paveen/RolePlaying/shared", model, size)
 # MODEL_DIR = "openchat/openchat_3.5"
-# MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"
-MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
+MODEL_DIR = "meta-llama/Llama-3.2-3B-Instruct"
+# MODEL_DIR = "meta-llama/Llama-3.1-8B-Instruct"
 # MODEL_DIR = "mistralai/Mistral-7B-Instruct-v0.3"
 # MODEL_DIR = "HuggingFaceH4/zephyr-7b-beta"
 # MODEL_DIR = "NousResearch/Hermes-3-Llama-3.2-3B"
