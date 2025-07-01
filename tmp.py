b963db4a2b6e99bbe5fc77d81126296c6a2dc5db
Paveen <paveenhuang@gmail.com>
2025-07-01 12:31:18 +0800

    Create tmp.py

---
+++ tmp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  1 12:30:54 2025
+
+@author: paveenhuang
+"""
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.3")
+
+text = "non-abstract-algebra expert"
+
+tokens = tokenizer.tokenize(text)
+
+print(tokens)
