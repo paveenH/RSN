d066d1d8d18de7f930b7f0a1e3f37d0f45d7d0d3
Paveen <paveenhuang@gmail.com>
2025-07-11 14:14:51 +0800

    Update get_answer_logits.py

--- get_answer_logits.py
+++ get_answer_logits.py
@@ -132,12 +132,12 @@ def main():
 
 if __name__ == "__main__":
     TASKS = ga.TASKS
-    MODEL = "deepseek"  # list of MMLU tasks
+    MODEL = "deepseek_base"  # list of MMLU tasks
     SIZE = "7B"
     TYPE = "non"
     SAVE = True
     ANS = f"answer_{TYPE}_logits"
-    MODEL_DIR = "deepseek-ai/deepseek-llm-7b-chat"
+    MODEL_DIR = "deepseek-ai/deepseek-llm-7b-base"
     print("model: ", MODEL)
     print("Loading model from:", MODEL_DIR)
 
