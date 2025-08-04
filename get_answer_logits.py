0c7910af3213001e723569cbc391fe94e4a14c5c
Paveen <paveenhuang@gmail.com>
2025-08-04 15:54:39 +0800

    Update get_answer_logits.py

--- get_answer_logits.py
+++ get_answer_logits.py
@@ -68,7 +68,7 @@ def make_characters(task_name: str, type_: str):
             # "person",
             # f"{task_name} student",
             # f"{task_name} expert",
-            "norole",
+            # "norole",
             "vanilla"
         ]
     else:
