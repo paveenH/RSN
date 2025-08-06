1da9e68c52f25a1b75c94b54d0afc9287bb69816
Paveen <paveenhuang@gmail.com>
2025-08-06 15:16:30 +0800

    Update get_answer.py

--- get_answer.py
+++ get_answer.py
@@ -48,7 +48,9 @@ def make_characters(task_name: str, type_: str):
         return [
             # f"non {task_name} expert",
             # f"{task_name} expert",
-            f"not an expert in {task_name}",
+            # f"not an expert in {task_name}", 
+            f"{task_name} student",
+            "person",
         ]
     else:
         return
