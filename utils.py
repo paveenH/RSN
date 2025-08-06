06bee603310120e91e897e2a5aecd94692ab7545
Paveen <paveenhuang@gmail.com>
2025-08-06 15:52:57 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ def make_characters(task_name: str, type_: str):
         return [
             # f"non {task_name} expert",
             # f"{task_name} expert",
-            f"not an expert in {task_name}",
+            # f"not an expert in {task_name}",
             f"{task_name} student",
             "person",
         ]
