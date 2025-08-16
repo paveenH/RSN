96b2ee90f1f2d500ba2a76a4860e661e74bea608
Paveen <paveenhuang@gmail.com>
2025-08-16 16:24:05 +0800

    Update get_answer_regenerate_logits.py

--- get_answer_regenerate_logits.py
+++ get_answer_regenerate_logits.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     parser.add_argument("--size", type=str, default="7B")
     parser.add_argument("--type", type=str, default="non")
     parser.add_argument("--percentage", type=float, default=0.5)
-    parser.add_argument("--configs", nargs="+", default=["4-16-22", "1-1-29"], help="List of alpha-start-end triplets, e.g. 4-16-22")
+    parser.add_argument("--configs", nargs="*", default=["4-16-22", "1-1-29"], help="List of alpha-start-end triplets, e.g. 4-16-22")
     parser.add_argument("--mask_type", type=str, default="nmd", help="Mask type to load: nmd or random")
     parser.add_argument("--abs", action="store_true")
     parser.add_argument("--ans_file", type=str, default="answer_mdf")
