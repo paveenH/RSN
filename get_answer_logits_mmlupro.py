7376a24bc9f4cc4a8e1e059df65305317cda016f
Paveen <paveenhuang@gmail.com>
2025-08-23 15:08:44 +0800

    Update get_answer_logits_mmlupro.py

--- get_answer_logits_mmlupro.py
+++ get_answer_logits_mmlupro.py
@@ -28,7 +28,7 @@ def main():
     vc.model.eval()
 
     # Load mmlupro json file
-    all_samples: List[dict] = utils.load_json(Path(MMLU_PRO_DIR))
+    all_samples: List[dict] = utils.load_json(MMLU_PRO_DIR)
 
     # group by "task"
     tasks = sorted({s["task"] for s in all_samples})
