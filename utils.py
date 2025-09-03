5763e5638892f226a1dbe90fd7a7d50373904c40
Paveen <paveenhuang@gmail.com>
2025-09-03 15:34:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -50,7 +50,7 @@ def make_characters(task_name="", type_="non"):
             "person",
             # "non expert", 
             # "expert",
-            # "neutral",
+            "neutral",
             # "vanilla",
             # "cot",
             # "medical expert",
