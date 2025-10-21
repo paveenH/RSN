19f2b18544c7eb30f22809fc27bf87db7d6f376b
Paveen <paveenhuang@gmail.com>
2025-10-21 18:13:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -72,15 +72,15 @@ def make_characters(task_name="", type_="non"):
     elif type_ == "non":
         task_name = task_name.replace("_", " ")
         return [
-            f"non {task_name} expert",
-            f"{task_name} expert",
+            # f"non {task_name} expert",
+            # f"{task_name} expert",
             # f"not an expert in {task_name}",
             # f"{task_name} student",
             # "person",
             # "non expert", 
             # "expert",
-            # "non medical expert", 
-            # "medical expert",
+            "non medical expert", 
+            "medical expert",
             # "confident",
             # "unconfident",
             # "neutral",
